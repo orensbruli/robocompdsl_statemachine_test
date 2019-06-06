130dcee01ecd57e841f237af10a66e62b5518743
Esteban Martinena Guerrero <orensbruli@gmail.com>
2019-06-06 17:28:02 +0200

    Python no smdsl dile example

---
+++ components/noSmdslTestPython/src/genericworker.py
@@ -0,0 +1,63 @@
+#
+# Copyright (C)
+<@@< 2019>@@>
+ by YOUR NAME HERE
+#
+#    This file is part of RoboComp
+#
+#    RoboComp is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    RoboComp is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys, Ice, os
+from PySide2 import QtWidgets, QtCore
+
+ROBOCOMP = ''
+try:
+	ROBOCOMP = os.environ['ROBOCOMP']
+except KeyError:
+	print '$ROBOCOMP environment variable not set, using the default value /opt/robocomp'
+	ROBOCOMP = '/opt/robocomp'
+
+preStr = "-I/opt/robocomp/interfaces/ -I"+ROBOCOMP+"/interfaces/ --all /opt/robocomp/interfaces/"
+Ice.loadSlice(preStr+"CommonBehavior.ice")
+import RoboCompCommonBehavior
+
+additionalPathStr = ''
+icePaths = [ '/opt/robocomp/interfaces' ]
+try:
+	SLICE_PATH = os.environ['SLICE_PATH'].split(':')
+	for p in SLICE_PATH:
+		icePaths.append(p)
+		additionalPathStr += ' -I' + p + ' '
+	icePaths.append('/opt/robocomp/interfaces')
+except:
+	print 'SLICE_PATH environment variable was not exported. Using only the default paths'
+	pass
+
+
+
+
+<@@<
+try:
+<TABHERE>from ui_mainUI import *
+except:
+<TABHERE>print "Can't import UI file. Did you run 'make'?"
+<TABHERE>sys.exit(-1)
+>@@>
+
+
+class GenericWorker(
+<@@<QtWidgets.QWidget>@@>
+):
+
+	kill = QtCore.Signal()
